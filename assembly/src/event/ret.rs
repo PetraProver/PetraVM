942b4d6d49d8279de21271d29b0c822c4595a894
Alonso Gonzalez <alonso.gon@gmail.com>
2025-02-25 14:43:02 +0100

    Add event folder

---
+++ assembly/src/event/ret.rs
@@ -0,0 +1,38 @@
+use crate::emulator::{Interpreter, StateChannel};
+
+use super::Event;
+
+#[derive(Debug)]
+pub struct RetEvent {
+    pc: u16,
+    fp: u16,
+    timestamp: u16,
+    fp_0_val: u16,
+    fp_1_val: u16,
+}
+
+impl RetEvent {
+    pub fn new(interpreter: &Interpreter) -> Self {
+        Self {
+            pc: interpreter.pc,
+            fp: interpreter.fp,
+            timestamp: interpreter.timestamp,
+            fp_0_val: interpreter.vrom[interpreter.fp as usize] as u16,
+            fp_1_val: interpreter.vrom[interpreter.fp as usize + 1] as u16,
+        }
+    }
+
+    pub fn generate_event(interpreter: &mut Interpreter) -> RetEvent {
+        interpreter.pc = interpreter.vrom[interpreter.fp as usize] as u16;
+        interpreter.fp = interpreter.vrom[interpreter.fp as usize + 1] as u16;
+        interpreter.timestamp = interpreter.timestamp + 1;
+        RetEvent::new(&interpreter)
+    }
+}
+
+impl Event for RetEvent {
+    fn fire(&self, state_channel: &mut StateChannel) {
+        state_channel.pull((self.pc, self.fp, self.timestamp));
+        state_channel.push((self.fp_0_val, self.fp_1_val, self.timestamp + 1));
+    }
+}
---
+++ assembly/src/event/sli.rs
@@ -0,0 +1,97 @@
+use crate::{
+    emulator::{Channel, Interpreter, StateChannel},
+    event::Event,
+};
+
+#[derive(Debug, Clone)]
+pub enum ShiftKind {
+    Left,
+    Right,
+}
+
+// Struture of an event for one of the shifts.
+#[derive(Debug, Clone)]
+pub struct SliEvent {
+    pc: u16,
+    fp: u16,
+    timestamp: u16,
+    dst: u32,
+    dst_val: u32,
+    src: u32,
+    src_val: u32,
+    shift: u32,
+    kind: ShiftKind,
+}
+
+impl SliEvent {
+    pub fn new(
+        pc: u16,
+        fp: u16,
+        timestamp: u16,
+        dst: u32,
+        dst_val: u32,
+        src: u32,
+        src_val: u32,
+        shift: u32,
+        kind: ShiftKind,
+    ) -> Self {
+        Self {
+            pc,
+            fp,
+            timestamp,
+            dst,
+            dst_val,
+            src,
+            src_val,
+            shift,
+            kind,
+        }
+    }
+
+    pub fn generate_event(
+        interpreter: &mut Interpreter,
+        dst: u32,
+        src: u32,
+        imm: u32,
+        kind: ShiftKind,
+    ) -> SliEvent {
+        println!("src = {}, vrom_size = {}", src, interpreter.vrom_size());
+        assert!((src as usize) < interpreter.vrom_size());
+        let src_val = interpreter.vrom[src as usize];
+        let new_val = if imm == 0 || imm >= 32 {
+            0
+        } else {
+            match kind {
+                ShiftKind::Left => src_val << imm,
+                ShiftKind::Right => src_val >> imm,
+            }
+        };
+        if dst as usize > interpreter.vrom_size() - 1 {
+            interpreter.extend_vrom(&vec![0; dst as usize - interpreter.vrom_size() + 1]);
+        }
+        let pc = interpreter.pc;
+        let timestamp = interpreter.timestamp;
+        interpreter.vrom[dst as usize] =  new_val;
+        interpreter.pc = pc + 1;
+        interpreter.timestamp = timestamp + 1;
+
+        SliEvent::new(
+            pc,
+            interpreter.fp,
+            timestamp,
+            dst,
+            new_val,
+            src,
+            src_val,
+            imm,
+            kind,
+        )
+    }
+}
+
+impl Event for SliEvent {
+    fn fire(&self, state_channel: &mut StateChannel) {
+        state_channel.pull((self.pc, self.fp, self.timestamp));
+        state_channel.push((self.pc + 1, self.fp, self.timestamp + 1));
+    }
+}
