e2d0ef677bcb8a51b1c900cc2cdc8e24265c412b
Robin Salen <salenrobin@gmail.com>
2025-03-08 10:02:32 -0500

    Add B32_MUL instruction

--- assembly/src/event/b32.rs
+++ assembly/src/event/b32.rs
@@ -2,8 +2,9 @@ use binius_field::{BinaryField16b, BinaryField32b};
 
 use super::{BinaryOperation, Event};
 use crate::{
-    fire_non_jump_event, impl_32b_immediate_binary_operation, impl_binary_operation,
-    impl_event_for_binary_operation, impl_immediate_binary_operation, G,
+    event::NonImmediateBinaryOperation, fire_non_jump_event, impl_32b_immediate_binary_operation,
+    impl_binary_operation, impl_event_for_binary_operation, impl_immediate_binary_operation,
+    impl_left_right_output_for_bin_op, G,
 };
 
 /// Event for XORI.
@@ -75,6 +76,86 @@ impl BinaryOperation for AndiEvent {
 impl_immediate_binary_operation!(AndiEvent);
 impl_event_for_binary_operation!(AndiEvent);
 
+/// Event for B32_MUL.
+#[derive(Debug, Default, Clone)]
+pub(crate) struct B32MulEvent {
+    timestamp: u32,
+    pc: BinaryField32b,
+    fp: u32,
+    dst: u16,
+    dst_val: u32,
+    src1: u16,
+    src1_val: u32,
+    src2: u16,
+    src2_val: u32,
+}
+
+impl B32MulEvent {
+    pub fn generate_event(
+        interpreter: &mut crate::emulator::Interpreter,
+        dst: BinaryField16b,
+        src1: BinaryField16b,
+        src2: BinaryField16b,
+    ) -> Self {
+        let src1_val = interpreter.vrom.get_u32(interpreter.fp ^ src1.val() as u32);
+        let src2_val = interpreter.vrom.get_u32(interpreter.fp ^ src2.val() as u32);
+        let dst_val = Self::operation(BinaryField32b::new(src1_val), BinaryField32b::new(src2_val));
+
+        let event = Self::new(
+            interpreter.timestamp,
+            interpreter.pc,
+            interpreter.fp,
+            dst.val(),
+            dst_val.val(),
+            src1.val(),
+            src1_val,
+            src2.val(),
+            src2_val,
+        );
+
+        interpreter
+            .vrom
+            .set_u32(interpreter.fp ^ dst.val() as u32, dst_val.val());
+        interpreter.incr_pc();
+
+        event
+    }
+}
+
+impl BinaryOperation for B32MulEvent {
+    #[inline(always)]
+    fn operation(val1: BinaryField32b, val2: BinaryField32b) -> BinaryField32b {
+        val1 * val2
+    }
+}
+
+impl Event for B32MulEvent {
+    fn fire(
+        &self,
+        channels: &mut crate::emulator::InterpreterChannels,
+        tables: &crate::emulator::InterpreterTables,
+    ) {
+        assert_eq!(
+            self.dst_val,
+            Self::operation(
+                BinaryField32b::new(self.src1_val),
+                BinaryField32b::new(self.src2_val)
+            )
+            .into()
+        );
+
+        channels
+            .state_channel
+            .pull((self.pc, self.fp, self.timestamp));
+        channels
+            .state_channel
+            .push((self.pc * G * G, self.fp, self.timestamp + 1));
+    }
+}
+
+impl_binary_operation!(B32MulEvent);
+
+/// Event for B32_MULI.
 #[derive(Debug, Default, Clone)]
 pub(crate) struct B32MuliEvent {
     timestamp: u32,
