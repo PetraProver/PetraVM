1148242756f7dc66a1ac9ebf3e791bef5b1bea0c
BGluth <gluthb@gmail.com>
2025-03-21 17:16:31 -0600

    Added `static_int_list_sum.asm`

---
+++ examples/static_int_list_sum.asm
@@ -0,0 +1,29 @@
+;; Rust equivalent:
+;; ------------
+;; // Note that in our case that the pointer to the list is always going to be a static list.
+;; fn static_int_list_sum(list: &[u32], list_size: usize, curr_sum: u32) -> u32 {
+;;   match list_size > 0 {
+;;   true => static_int_list_sum(&list[1..], list_size - 1, curr_sum + list[0]),
+;;   false => curr_sum,
+;; }
+;; ------------
+
+static_int_list_sum:
+    ;; Frame:
+    ;; Slot 0: Return PC
+    ;; Slot 1: Return FP
+    ;; Slot 2: Arg: list addr
+    ;; Slot 3: Arg: list_size
+    ;; Slot 4: Arg: curr_sum
+    ;; Slot 5: ND Local: Next FP
+
+    BNZ @3, list_size_gt_0
+    MVI.H @5, @4 ;; return curr_sum
+    RET
+
+list_size_gt_0:
+    ;; Recursively call this function again
+    MOV.W @5[2], @2[1] ;; list[1..]
+    SUB @5[3], @3, #1 ;; list_size - 1
+    ADD @5[4], @4, @2[0] ;; curr_sum + list[0]
+    TAILI static_int_list_sum, @5 ;; static_int_list_sum(&list[1..], list_size - 1, curr_sum + list[0])
