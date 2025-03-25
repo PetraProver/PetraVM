8bce6c87483ef842e2496d79d108f748ef7f96da
Alonso Gonzalez <alonso.gon@gmail.com>
2025-03-25 10:37:21 +0100

    Minor

--- assembly/src/execution/emulator_arithmetization.rs
+++ assembly/src/execution/emulator_arithmetization.rs
@@ -14,8 +14,8 @@ pub mod arithmetization {
     pub struct ZCrayTable {
         pub(crate) add_table: AddTable,
         pub(crate) ret_table: RetTable,
-        // pub(crate) bnz_table: BnzTable,
-        // pub(crate) bz_table: BzTable,
+        pub(crate) bnz_table: BnzTable,
+        pub(crate) bz_table: BzTable,
         pub(crate) state_channel: ChannelId,
         pub(crate) prom_channel: ChannelId,
         pub(crate) vrom_channel: ChannelId,
@@ -29,8 +29,8 @@ pub mod arithmetization {
             Self {
                 add_table: AddTable::new(cs, state_channel, vrom_channel, prom_channel),
                 ret_table: RetTable::new(cs, state_channel, vrom_channel, prom_channel),
-                // bnz_table: BnzTable::new(cs, state_channel, vrom_channel, prom_channel),
-                // bz_table: BzTable::new(cs, state_channel, vrom_channel, prom_channel),
+                bnz_table: BnzTable::new(cs, state_channel, vrom_channel, prom_channel),
+                bz_table: BzTable::new(cs, state_channel, vrom_channel, prom_channel),
                 state_channel,
                 prom_channel,
                 vrom_channel,
@@ -48,7 +48,8 @@ pub mod arithmetization {
             println!("bz:{:?}", trace.bz);
             witness.fill_table_sequential(&self.add_table, &trace.add)?;
             witness.fill_table_sequential(&self.ret_table, &trace.ret)?;
-            // witness.fill_table_sequential(&self.bnz_table, &trace.bnz)?;
+            witness.fill_table_sequential(&self.bnz_table, &trace.bnz)?;
+            witness.fill_table_sequential(&self.bz_table, &trace.bz)?;
             Ok(())
         }
     }
