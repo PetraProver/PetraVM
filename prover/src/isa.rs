33f88c7f8f1c94b37fc64b196797061f6e7220e3
Robin Salen <salenrobin@gmail.com>
2025-04-06 17:05:13 -0400

    Add macro for global ISA declaration

--- prover/src/isa.rs
+++ prover/src/isa.rs
@@ -54,22 +54,51 @@ macro_rules! register_table {
     };
 }
 
-// TODO: implement when possible
-pub struct RecursionISA;
+/// Creates a new ISA and registers all its instruction tables.
+///
+/// # Example
+///
+/// ```ignore
+/// define_isa!(MinimalISA => (LdiTable, ldi_events), (RetTable, ret_events));
+/// ```
+#[macro_export]
+macro_rules! define_isa {
+    (
+        $(#[$doc:meta])*
+        $isa_ty:ident => [ $( ($table_ty:ty, $accessor:ident) ),* $(,)? ]
+    ) => {
+        $(#[$doc])*
+        pub struct $isa_ty;
 
-/// The main Instruction Set Architecture (ISA) for the zCray Virtual Machine,
-/// supporting all existing instructions.
-pub struct GenericISA;
-
-impl ISA for GenericISA {
-    fn register_tables(
-        &self,
-        cs: &mut ConstraintSystem,
-        channels: &Channels,
-    ) -> Vec<Box<dyn FillableTable>> {
-        vec![
-            register_table!(LdiTable, ldi_events, cs, channels),
-            register_table!(RetTable, ret_events, cs, channels),
-        ]
-    }
+        impl $crate::isa::ISA for $isa_ty {
+            fn register_tables(
+                &self,
+                cs: &mut binius_m3::builder::ConstraintSystem,
+                channels: &$crate::channels::Channels,
+            ) -> Vec<Box<dyn $crate::table::FillableTable>> {
+                vec![
+                    $(
+                        $crate::register_table!($table_ty, $accessor, cs, channels)
+                    ),*
+                ]
+            }
+        }
+    };
 }
+
+// TODO: Implement Recursion VM whenever possible.
+// Needs to implement #79.
+
+// define_isa!(
+//     /// A minimal ISA for the zCray Virtual Machine,
+//     /// tailored for efficient recursion.
+//     RecursionISA => []
+// );
+
+define_isa!(
+    /// The main Instruction Set Architecture (ISA) for the zCray Virtual Machine,
+    /// supporting all existing instructions.
+    GenericISA => [
+    (LdiTable, ldi_events),
+    (RetTable, ret_events),
+]);
