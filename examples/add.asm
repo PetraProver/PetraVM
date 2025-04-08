29f757369b96dd3b263a882b0b8cec57809e00d9
BGluth <gluthb@gmail.com>
2025-04-08 09:43:15 -0600

    Logic for add ASM test

--- examples/add.asm
+++ examples/add.asm
@@ -1,8 +1,3 @@
-;; Rust equivalent:
-;; ------------
-;; return 2 + 6
-;; ------------
-
 #[framesize(0x4)]
 add:
     ;; Frame
@@ -10,8 +5,7 @@ add:
     ;; Slot @1: Return FP
     ;; Slot @2: Return value (2 + 6)
     ;; Slot @3: Local: 2
-    ;; Slot @4: Local: 2 + 6
 
-    MVI.H @3, #2    ;; x = 2
+    LDI.W @3, #2      ;; x = 2
     ADDI @2, @3, #6 ;; x + 6
     RET
