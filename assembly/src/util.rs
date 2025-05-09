aaf054724d25865e616431ed8a2c8f6f569e91c1
Robin Salen <salenrobin@gmail.com>
2025-05-09 19:17:03 -0400

    Change default tracing subscriber

--- assembly/src/util.rs
+++ assembly/src/util.rs
@@ -1,9 +1,18 @@
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{fmt::format::FmtSpan, layer::SubscriberExt, EnvFilter};
 
 /// Initializes the global tracing subscriber.
 ///
 /// The default `Level` is `INFO`. It can be overridden with `RUSTFLAGS`.
 pub fn init_logger() {
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    tracing_subscriber::fmt().with_env_filter(filter).init();
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_span_events(FmtSpan::CLOSE)
+        .with_target(false);
+
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(fmt_layer)
+        .init();
 }
