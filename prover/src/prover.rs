7c2e437df87a3ad25ab94fd41084d835ce44ec4b
Sai Deng <deng.sai@gmail.com>
2025-05-22 14:14:00 -0700

    fix

--- prover/src/prover.rs
+++ prover/src/prover.rs
@@ -48,10 +48,7 @@ impl Prover {
         allocator: &'a Bump,
     ) -> Result<WitnessIndex<'_, 'a, ProverPackedField>> {
         // Build the witness structure
-        let mut witness = self
-            .circuit
-            .cs
-            .build_witness::<ProverPackedField>(allocator);
+        let mut witness = WitnessIndex::new(&self.circuit.cs, allocator);
 
         // Fill all table witnesses in sequence
 
