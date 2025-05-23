cfd62afed9330c30ef5f9f5102867cd423d27523
Aliaksei Dziadziuk <adziadziuk@irreducible.com>
2025-05-23 11:25:21 +0200

    fix

--- assembly/src/util.rs
+++ assembly/src/util.rs
@@ -1,14 +1,19 @@
+use binius_utils::rayon::adjust_thread_pool;
 use tracing_forest::ForestLayer;
+use tracing_profile::init_tracing;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
-
 /// Initializes the global tracing subscriber.
 ///
 /// The default `Level` is `INFO`. It can be overridden with `RUSTFLAGS`.
-pub fn init_logger() {
-    if cfg!(feature = "tracing-profile") || cfg!(feature = "perfetto") {
-        use tracing_profile::init_tracing;
-        let _guard = init_tracing().expect("failed to initialize tracing");
+pub fn init_logger() -> Option<impl Drop> {
+    if cfg!(feature = "tracing-profile") {
+        adjust_thread_pool()
+            .as_ref()
+            .expect("failed to init thread pool");
+
+        let guard = init_tracing().expect("failed to initialize tracing");
+        return Some(guard);
     } else {
         let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
@@ -17,4 +22,5 @@ pub fn init_logger() {
             .with(ForestLayer::default())
             .init();
     }
+    None
 }
