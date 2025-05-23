5b7cff5c39002d83ae81aac542950d9c3dccc24d
Aliaksei Dziadziuk <adziadziuk@irreducible.com>
2025-05-23 10:12:06 +0200

    tracing-profile integration

--- assembly/src/util.rs
+++ assembly/src/util.rs
@@ -6,10 +6,15 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 ///
 /// The default `Level` is `INFO`. It can be overridden with `RUSTFLAGS`.
 pub fn init_logger() {
-    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    if cfg!(feature = "tracing-profile") || cfg!(feature = "perfetto") {
+        use tracing_profile::init_tracing;
+        let _guard = init_tracing().expect("failed to initialize tracing");
+    } else {
+        let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
-    tracing_subscriber::registry()
-        .with(filter)
-        .with(ForestLayer::default())
-        .init();
+        tracing_subscriber::registry()
+            .with(filter)
+            .with(ForestLayer::default())
+            .init();
+    }
 }
